a9eedd9c0e94d962e08700c858899e693d5d9812
A-Coutinho <antoniomiguelcoutinho@gmail.com>
2023-10-31 23:52:06 +0000

    migrate to mssql - changes in db connection

--- backend/index.js
+++ backend/index.js
@@ -2,57 +2,114 @@ import express from "express";
 import mysql from 'mysql';
 import cors from 'cors';
 import dotenv from "dotenv";
+import sql from "mssql";
 
 dotenv.config();
-const app = express(); 
+const app = express();
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
+var sqlConfig = {
+    server: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DABA,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS
-});
+    password: process.env.MYSQL_PASS,
+    port: 65500,
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+    }
+};
 
-console.log(db.config.password);
 
+const request = new sql.Request();
 app.get("/", (req, res) => {
     res.json("hello this is backend");
 });
 
 app.get("/totalPoints", (req, res) => {
     const q = "select * from vw_totalpoints";
-    db.query(q, (err,data) => { if(err) return res.json(err); else return res.json(data); })
+    sql.connect(sqlConfig, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.query(q, function (err, recordset) {
+            if (err) console.log(err)
+            console.log(recordset.recordset)
+            res.send(recordset.recordset);
+        });
+    });
 });
 
 app.get("/roundsWinners", (req, res) => {
     const q = "select * from vw_roundswinners";
-    db.query(q, (err,data) => { if(err) return res.json(err); else return res.json(data); })
+    sql.connect(sqlConfig, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.query(q, function (err, recordset) {
+            if (err) console.log(err)
+            res.send(recordset.recordset);
+        });
+    });
 });
 
 app.get("/roundsWinnersByTimes", (req, res) => {
     const q = "select name, count(name) as won from vw_roundswinners group by name order by won desc";
-    db.query(q, (err,data) => { if(err) return res.json(err); else return res.json(data); })
+    sql.connect(sqlConfig, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.query(q, function (err, recordset) {
+            if (err) console.log(err)
+            res.send(recordset.recordset);
+        });
+    });
 });
 
 app.get("/listOfPlayers", (req, res) => {
     const q = "select id, name, username from players order by id";
-    db.query(q, (err,data) => { if(err) return res.json(err); else return res.json(data); })
+    sql.connect(sqlConfig, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.query(q, function (err, recordset) {
+            if (err) console.log(err)
+            res.send(recordset.recordset);
+        });
+    });
 });
 
 app.get("/playerRounds/:player", (req, res) => {
-    const q = "select number, points from rounds where playerid = ?";
-    db.query(q, [req.params.player], (err,data) => { if(err) return res.json(err); else return res.json(data); })
+    const q = "select number, points from rounds where playerid = @playerparam";
+    sql.connect(sqlConfig, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.input('playerparam', sql.VarChar, req.params.player).query(q, function (err, recordset) {
+            if (err) console.log(err)
+            res.send(recordset.recordset);
+        });
+    });
 });
 
 app.get("/players/:player", (req, res) => {
-    const q = "select name from players where id = ?";
-    db.query(q, [req.params.player], (err,data) => { if(err) return res.json(err); else return res.json(data); })
+    const q = "select name from players where id = @playerparam";
+    sql.connect(sqlConfig, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.input('playerparam', sql.VarChar, req.params.player).query(q, function (err, recordset) {
+            if (err) console.log(err)
+            res.send(recordset.recordset);
+        });
+    });
 });
 
 app.get("/allRounds/:round", (req, res) => {
-    const q = "select p.NAME, p.USERNAME, r.points from rounds r, players p where r.playerid = p.id and r.number = ? order by r.points desc;";
-    db.query(q, [req.params.round], (err,data) => { if(err) return res.json(err); else return res.json(data); })
+    const q = "select p.NAME, p.USERNAME, r.points from rounds r, players p where r.playerid = p.id and r.number = @roundparam order by r.points desc;";
+    sql.connect(sqlConfig, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.input('roundparam', sql.VarChar, req.params.round).query(q, function (err, recordset) {
+            if (err) console.log(err)
+
+            res.send(recordset.recordset);
+        });
+    });
 });
 
 app.listen(8800, () => {
